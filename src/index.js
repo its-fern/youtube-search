5f2893dab7bbcdcaa7030bd55e7e36135e73ccb9
Fernando Ramirez <fernando.ramirez@blueapron.com>
2017-02-11 11:44:21 -0500

    Section 1 - Lecture 15
    Basic search bar.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = /*<API KEY from Google Dev Tools>*/;
+
 // Create a new component. This component should
 // produce some html
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+  <div>
+    <SearchBar />
+  </div>
+  );
 }
 
 // Take this component generated HTML and put it in
