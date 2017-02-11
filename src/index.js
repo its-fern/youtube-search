10bb019a7ecaa9cf41e1c98fbc433409c1618d27
Fernando Ramirez <fernando.ramirez@blueapron.com>
2017-02-11 13:38:15 -0500

    Section 2 - Lecture 23
    Create video list component, render number of loaded videos. Use
    a component in place of a function on index.js.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
+import VideoList from './components/video_list';
 import SearchBar from './components/search_bar';
 import config from './config'
 
@@ -8,12 +10,24 @@ const API_KEY = config.GOOGLE_API_KEY;
 
 // Create a new component. This component should
 // produce some html
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 // Take this component generated HTML and put it in
