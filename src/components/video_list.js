4eca3bebcec305f4b9964c5878469d2d0cf7bd42
Fernando Ramirez <fernando.ramirez@blueapron.com>
2017-02-11 14:31:16 -0500

    Section 2 - Lecture 29
    Main video and list of videos now render. You can click on individual
    videos to view them in the main details.

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,7 +3,12 @@ import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
   const videoItems = props.videos.map((vid) => {
-    return <VideoListItem video={vid} />
+    return (
+      <VideoListItem
+        onVideoSelect={props.onVideoSelect}
+        key={vid.etag}
+        video={vid} />
+    );
   });
 
   return (
