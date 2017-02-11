0b2547cb4556f72e555e208ef47e5256907aafc8
Fernando Ramirez <fernando.ramirez@blueapron.com>
2017-02-11 13:45:02 -0500

    Section 2 - Lecture 25
    Render list of videos.

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
+  const videoItems = props.videos.map((vid) => {
+    return <VideoListItem video={vid} />
+  });
+
   return (
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+      {videoItems}
     </ul>
   );
 };
